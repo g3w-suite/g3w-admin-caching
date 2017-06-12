c1dff581ec908c24a9243669b011072a2a21c8be
wlorenzetti <lorenzetti@gis3w.it>
2017-06-12 10:00:33 +0200

    cu: new dumpdata fo mapcontrols.

--- models.py
+++ models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+
+class G3WCachingLayer(models.Model):
+    """
+    Layer to cache for every g3wsuite map module.
+    """
+    app_name = models.CharField(max_length=255)
+    layer_id = models.IntegerField()
+
