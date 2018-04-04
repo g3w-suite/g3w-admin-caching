50cc961a7e9dc4948fc123050713de2355041d0f
Walter Lorenzetti <lorenzetti@gis3w.it>
2018-04-04 15:45:00 +0200

    cu: tilestache api remove from login_required

--- apiurls.py
+++ apiurls.py
@@ -1,9 +1,8 @@
 from django.conf.urls import url
-from django.contrib.auth.decorators import login_required
 from .views import TileStacheTileApiView
 
 
 urlpatterns = [
     url(r'^api/(?P<layer_name>[-\w]+)/(?P<z>\d+)/(?P<x>\d+)/(?P<y>\d+).(?P<extension>\w+)$',
-        login_required(TileStacheTileApiView.as_view()), name='caching-api-tile')
+        TileStacheTileApiView.as_view(), name='caching-api-tile')
 ]
